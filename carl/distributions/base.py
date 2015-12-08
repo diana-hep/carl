06ef9dcca8d043373d60214de88ee37b03d9e209
Gilles Louppe <g.louppe@gmail.com>
2015-12-08 05:16:55 +0100

    Remove utils

--- carl/distributions/base.py
+++ carl/distributions/base.py
@@ -9,10 +9,32 @@ from sklearn.utils import check_random_state
 
 import theano
 import theano.tensor as T
-
-from ..utils import check_parameter
+from theano import Constant
+from theano import Variable
+from theano.gof import graph
+from theano.tensor.sharedvar import SharedVariable
 
 # ???: define the bounds of the parameters
+
+def check_parameter(name, value):
+    # Accept expressions only if they depend on bound variables
+    if isinstance(value, Variable):
+        inputs = graph.inputs([value])
+        for var in inputs:
+            if (not isinstance(var, SharedVariable) and
+                not isinstance(var, Constant)):
+                raise ValueError("Variable {} is free, or depends on a "
+                                 "free variable.".format(value))
+
+    # Accept constants
+    elif isinstance(value, Constant):
+        pass
+
+    # Accept raw values, and store them in a shared variable
+    else:
+        value = theano.shared(value, name=name)
+
+    return value
 
 
 class DistributionMixin(BaseEstimator):
