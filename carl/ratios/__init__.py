a7abc52fd5e2920070f00678cf788d6bcf1e6ed0
Gilles Louppe <g.louppe@gmail.com>
2015-12-08 05:10:52 +0100

    Structure for density ratio estimators

---
+++ carl/ratios/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+# Carl is free software; you can redistribute it and/or modify it
+# under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+"""Density ratio estimators."""
