f91c8787b57db545cde5d5532cb9a35369739845
Gilles Louppe <g.louppe@gmail.com>
2016-02-22 22:18:03 +0100

    Added g-and-k distribution

---
+++ carl/data/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Carl is free software; you can redistribute it and/or modify it
+# under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+from .gk import GK
+
+__all__ = ("GK")
---
+++ carl/data/gk.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Carl is free software; you can redistribute it and/or modify it
+# under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+import numpy as np
+
+from sklearn.utils import check_random_state
+
+from ..distributions import TheanoDistribution
+from ..distributions import Normal
+
+
+class GK(TheanoDistribution):
+    def __init__(self, A, B, g, k, c=0.8, random_state=None):
+        super(GK, self).__init__(A=A, B=B, g=g, k=k, c=c,
+                                 random_state=random_state)
+
+    def rvs(self, n_samples, **kwargs):
+        rng = check_random_state(self.random_state)
+
+        A = self.A.eval()  # pass kwargs?
+        B = self.B.eval()
+        g = self.g.eval()
+        k = self.k.eval()
+        c = self.c.eval()
+
+        z = rng.randn(n_samples).reshape(-1, 1)
+
+        Q = A + B * (1 + c * (1 - np.exp(-g * z)) /
+                             (1 + np.exp(-g * z))) * ((1 + z ** 2) ** k) * z
+
+        return Q
