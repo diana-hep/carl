caa2943acff7c434eb4c2d2eac43a8a5b8cfded6
Gilles Louppe <g.louppe@gmail.com>
2016-04-19 11:24:32 +0200

    Remove files

--- tests/data/test_ricker.py
+++
@@ -1,9 +0,0 @@
-# Carl is free software; you can redistribute it and/or modify it
-# under the terms of the Revised BSD License; see LICENSE file for
-# more details.
-
-from carl.data import Ricker
-
-
-def test_ricker():
-    pass  # XXX
